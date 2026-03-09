fc777406e3783593238041c48f53a44f080337be
jhansinyathari <jhansinyathari@gmail.com>
2026-03-09 12:58:32 -0700

    Assignmet 1

---
+++ typescript-training/data-types/assignment1.ts
@@ -0,0 +1,88 @@
+//Temperature of a city in degrees Celsius: 25.5
+let temperature: number = 25.5;
+console.log("temperature:", temperature);
+
+//2. Whether a customer has placed an order: true or false
+let order: boolean = true;
+console.log("placeorder:",order);
+
+//3. Person's phone number: "123-456-7890"
+let phonenumber: string = "123-456-7890";
+console.log("phone number:", phonenumber);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let bankbalance: number = 100.50;
+console.log("total balance:", bankbalance);
+
+//5. Person's email address: "john.doe@example.com"
+let email: string = "john.doe@example.com";
+console.log("Email:", email);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let latitude: number = 37.7749
+let longitude: number = -122.4194
+console.log("location coordinates:", latitude, longitude);
+
+
+//7. Person's marital status: true or false
+let Married: boolean = false;
+console.log("Marital Status:", Married);
+
+//8. Person's occupation: "Software Engineer"
+let occupation: string = "Software Engineer";
+console.log("Occupation:", occupation);
+
+//9. Person's favourite colour: "Blue"
+let favouritecolour: string = "Black";
+console.log("Favourite Colour:", favouritecolour);
+
+//10.Current year
+let currentyear: number = 2026;
+console.log("Current year:", currentyear);
+
+//11.Number of followers on a social media platform: 1,000,000
+let followers: number = 1000000;
+console.log("Followers:", followers);
+
+//12.Rating of a movie: 7.5
+let movierating: number = 7.5;
+console.log("Movie Rating:", movierating);
+
+//13.Person's blood type: 'A'
+let bloodtype: string = "A";
+console.log("Blood type:", bloodtype);
+
+
+//14.Title of a book: "To Kill a Mockingbird"
+let booktitle: string = "To Kill a Mockingbird";
+console.log("Book title:", booktitle);
+
+//15.Number of employees in a company: 500
+let noofEmployees: number = 500;
+console.log("Number of employees:", noofEmployees);
+
+//16.Time of an event: 2:30 PM
+let eventtime: string = "2:30 PM";
+console.log("Event Time:", eventtime);
+
+//17.Name of a country:
+let countryname: string = "INDIA";
+console.log("Country:", countryname);
+
+//18.Person's eye colour: "brown"
+let eyecolour: string = "Brown";
+console.log("Eye color:", eyecolour);
+
+//19.Person's birthplace: "New York City"
+let birthplace: string = "New York City";
+console.log("Birth Place:", birthplace);
+
+//20. Distance between two cities: 200.5
+let distancebetweencities: number = 200.5;
+console.log("Distance Between Cities:", distancebetweencities);
+
+
+
+
+
+
