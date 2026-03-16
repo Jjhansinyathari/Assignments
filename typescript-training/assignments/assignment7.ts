ca03b6835414847533981183089733f36ba375cb
jhansinyathari <jhansinyathari@gmail.com>
2026-03-16 13:40:19 -0700

    assignment7

---
+++ typescript-training/assignments/assignment7.ts
@@ -0,0 +1,25 @@
+let originalString: string = "Java programming is fun and challenging";
+
+console.log(" Sentence:");
+console.log(originalString);
+
+// 1. Count total number of words
+let words: string[] = originalString.split(" ");
+let wordCount: number = words.length;
+
+console.log("Total number of words:");
+console.log(wordCount);
+
+// 2. Print words in reverse order
+let reverseWords: string[] = [...words].reverse();
+
+console.log("\nSentence words in reverse order:");
+console.log(reverseWords.join(" "));
+
+// 3. Convert first character of each word to uppercase
+let upperCaseString: string = originalString.toUpperCase(); 
+    console.log("upperCaseStringis:'"+upperCaseString+ "'");
+
+
+console.log("\nSentence with first letter of each word in uppercase:");
+console.log(upperCaseString);g
